a7625ba7bd00123e3ec67d4360c3316f9675d5da
Manik Kulkarni <manik5992@gmail.com>
2022-06-17 23:26:49 +0530

    Day 1

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello Nodejs");
+//Creating a node js server
+
+const http = require('http')
+
+const app = http.createServer((req, res) => {
+  console.log(req)
+})
+
+app.listen(3000)
--- index.js
+++ index.js
@@ -1,6 +1,47 @@
-function sum(num1, num2) {
-  var sum = num1 + num2;
-  return sum;
-}
+//JS
 
-console.log(sum(10, 20));
+//  Normal Function defintion
+// function sum(num1, num2) {
+//   var sum = num1 + num2;
+//   return sum;
+// }
+
+// console.log(sum(10, 20));
+
+// Arrow function defintion
+// const printData = (name, age) => {
+//   return `My name is ${name} and I am ${age} years old`;
+// };
+
+// console.log(printData("Manik Kulkarni", 22));
+
+//Objects
+// const personObj = {
+//   name: 'Manik',
+//   age: 22,
+//   greet() {
+//     console.log(`Hi, I am ${this.name}`)
+//   },
+// }
+
+// personObj.greet()
+
+//Arrays
+
+// const hobbies = ['Sports', 'Reading Novels', 'Coding']
+// for (let hobby of hobbies) console.log(hobby)
+
+//map()
+
+// console.log(hobbies.map((hobby) => 'Hobby: ' + hobby))
+
+// const [hobby1, hobby2] = hobbies  //Destructuring
+// console.log(hobby1, hobby2)
+
+// const numbersArray = [1, 2, 3, 4, 5]
+// const [a, b, c] = numbersArray
+// console.log(a - b * c)
+
+// setTimeout(() => {
+//   console.log('Hello World')
+// }, 3000)
