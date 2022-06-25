f91fe176b83546c25bf46473f978eefee4fcc36a
Manik Kulkarni <manik5992@gmail.com>
2022-06-25 07:39:14 +0530

    Commit 2

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 const http = require('http')
 
 const app = http.createServer((req, res) => {
-  console.log(req)
+  res.write('<h1>Hello Server</h1>')
+  res.write('<p>This is para tag</p>')
 })
 
 app.listen(3000)
